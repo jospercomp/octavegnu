6332f93e80e1f963f8ee3f43a19b5d5be3f8b2c0
jospercomp <58311484+jospercomp@users.noreply.github.com>
2021-05-26 08:05:05 -0400

    Add files via upload

---
+++ Otros/Matriz_Rows_Diferentes_Unique.m
@@ -0,0 +1,18 @@
+clear all; close all; clc;
+%Matriz con filas unicas
+%randi([a b],rows,columns)
+matriz=randi([0 1],10,5)
+matrizu=[];
+for i=1:size(matriz,1)
+  unico=true;
+  for j=i+1:size(matriz,1)
+    if isequal(matriz(i,:),matriz(j,:)) 
+      unico=false;
+      break;
+    endif
+  endfor
+  if unico
+  matrizu=[matrizu; matriz(i,:)];
+  endif  
+endfor
+matrizu
