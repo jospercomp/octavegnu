f4b51351d166b25fc4074ac5d48556111f14f169
jospercomp <58311484+jospercomp@users.noreply.github.com>
2021-06-12 21:39:37 -0400

    Add files via upload

---
+++ demonstrations/uitable3_demonstration.m
@@ -0,0 +1,22 @@
+clear all; close all; clc;
+col={'height','age','weight'};
+row={'first','second','third'};
+dat={'177','21','67';
+     '178','22','67';
+     '179','21','67'};
+uitable('columnname',col,'rowname',row, ... 
+'position',[20 20 287 100],'data',dat)
+%%%%%%%%%%%%%%%%%%%
+f=figure('Name','Second UItable','Position',...
+[100 100 450 200]);
+x=[1 2 3 4 5]; 
+y=sin(x);
+z=cos(x);
+w=2*sin(2.*x);
+dat=num2cell([x' y' z' w']); 
+columnname={'Input','Left Machine', ...
+'Right Machine','Middle Machine'};
+t=uitable('parent',f,'units','normalized', ...
+'Position',[.1 .1 0.75 .8],'Data',dat, ...
+'columnname', columnname, ...
+'columnformat',{{'numeric'},{'numeric'},{'numeric'},{'numeric'}});
